0be62d4d338d6189ac071fd54558eac965818a68
Oleg <93030888+multik8711@users.noreply.github.com>
2022-01-07 00:57:12 +0300

    finallity

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,15 @@
-console.log('file 1');
+var burger = document.getElementById('burg');
+const li = document.getElementById('list');
+
+
+
+burger.addEventListener('click', function() {
+    if (this.click) {
+        li.classList.add('Disp')
+    }
+})
+li.addEventListener('click', function(e) {
+    if (this.click) {
+        li.classList.remove('Disp')
+    }
+})
